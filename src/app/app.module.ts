9c8bf1e75fa166c7305321d88fc738022134b4f4
Leonidas Yopan <leonidas@prevision.com.br>
2021-04-29 23:56:21 -0300

    added a contacts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
