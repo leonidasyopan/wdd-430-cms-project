941cb40544b845607d21752e3d55f39ccb59b661
Leonidas Yopan <leonidas@prevision.com.br>
2021-04-29 23:57:53 -0300

    generated a contact-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContactsComponent
+    ContactsComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule
