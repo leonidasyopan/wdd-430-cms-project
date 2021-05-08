ab85017ee3b726978e9c4b351fdfafe5e4b74ac1
Leonidas Yopan <leonidasyopan@gmail.com>
2021-05-08 02:09:03 -0300

    WIP on handling data binding

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Contact } from '../contact.model'
 
@@ -8,6 +8,8 @@ import { Contact } from '../contact.model'
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
+
   contacts: Contact[] = [
     new Contact(
       "1",
@@ -30,6 +32,10 @@ export class ContactListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onContactSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact)
   }
 
 }
