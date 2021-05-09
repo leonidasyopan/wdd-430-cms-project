99e7efdf3fadcd33f85fa1a6327ad99b2f87308b
Leonidas Yopan <leonidas@prevision.com.br>
2021-05-08 22:45:10 -0300

    changed main page back to contacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'cms';
 
-  selectedFeature = 'message-list';
+  selectedFeature = 'contacts';
 
   switchView(selectedFeature: string) {
     this.selectedFeature = selectedFeature;
