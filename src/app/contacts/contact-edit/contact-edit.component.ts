830012fc9a1c8e36a6dd67fa8543de99714d45da
Leonidas Yopan <leonidasyopan@gmail.com>
2021-06-11 20:38:03 -0300

    Implemented logic to edit Contact

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-edit',
@@ -9,22 +11,54 @@ import { Contact } from '../contact.model';
 })
 export class ContactEditComponent implements OnInit {
 
-  groupContacts;
+  id: string;
 
+  originalContact: Contact;
   contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
 
-  constructor() { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private contactService: ContactService,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = String(+params['id']);
+          this.editMode = params['id'] != null || params['id'] != undefined;
+
+          if(!this.id) return;
+
+          this.originalContact = this.contactService.getContact(params['id'])
+          if(!this.originalContact) return;
+
+          this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+          if(this.contact.group?.length > 0) {
+            this.groupContacts = JSON.parse(JSON.stringify(this.contact.group));
+          }
+        }
+      )
   }
 
   onCancel() {
-    console.log("cancel");
+    this.router.navigate(['/contacts']);
   }
 
   onSubmit(form: NgForm) {
-    console.log("cancel");
-    console.log(form);
+    const value = form.value;
+    const newContact = new Contact(this.id, value.name, value.email, value.phone, value.imageUrl, []);
+    if(this.editMode) {
+      newContact.group = this.groupContacts;
+      this.contactService.updateContact(this.originalContact, newContact);
+    } else {
+      this.contactService.addContact(newContact);
+    }
+    this.router.navigate(['/contacts']);
   }
-
 }
