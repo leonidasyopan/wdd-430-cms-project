81d70f8574c41de2a94e5c4b0765791d17942f2c
Leonidas Yopan <leonidasyopan@gmail.com>
2021-06-29 23:55:45 -0300

    WIP started working on the routes -> Get

--- server/routes/documents.js
+++ server/routes/documents.js
@@ -1,4 +1,17 @@
+const sequenceGenerator = require('./sequenceGenerator');
+const Document = require('../models/document');
+
 var express = require('express');
 var router = express.Router();
 module.exports = router;
 
+
+router.get('/', (req, res, next) => {
+  Document.find().then(documents => {
+    res.status(200).json(documents);
+  }).catch(error => {
+    res.status(500).json({
+      message: "Error trying to fetch Documents"
+    });
+  })
+});
